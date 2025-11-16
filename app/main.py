1e5d06a5df0fa7b9618bae7b614be3edee97eebc
Miguel Angel Quintanilla <miguel.angel.quintanilla@darwinex.com>
2025-11-16 16:42:54 +0100

    Add CORS configuration to FastAPI backend for web integration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import auth_router, garment_router, look_router, test_router, healtcheck_router
 
+
 app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["*"],  # Puedes especificar dominios en vez de "*"
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 # Montaje de routers
 #app.include_router(auth_router.router, prefix="/api/v1/auth")
